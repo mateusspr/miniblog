cce17a22c3f9652d38269c9465d3f5f6d5897516
Mateus Rodrigues <mateus.pereira.maia94@gmail.com>
2024-05-09 11:36:33 -0300

    feat: adding context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import NavBar from './components/NavBar/NavBar'
@@ -7,23 +8,27 @@ import Footer from './components/Footer/Footer'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 
+import { AuthProvider } from './context/AuthContext'
+
 function App() {
 
   return (
     <>
       <div className='App'>
-        <BrowserRouter>
-          <NavBar />
-          <div className="container">
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <NavBar />
+            <div className="container">
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
       </div>
     </>
   )
